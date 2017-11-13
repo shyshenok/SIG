e8a5f4e8350e60a2c238aaab045fd993257f8b48
shyshenok <shyshenoka@gmail.com>
2017-11-13 16:03:10 +0200

    Add favicon

--- src/js/main.js
+++ src/js/main.js
@@ -5,15 +5,15 @@
 //= ../../bower_components/popper.js/dist/umd/popper.js
 
 // Импортируем необходимые js-файлы Bootstrap 4
-// = ../../bower_components/bootstrap/js/dist/util.js
-// = ../../bower_components/bootstrap/js/dist/alert.js
-// = ../../bower_components/bootstrap/js/dist/button.js
-// = ../../bower_components/bootstrap/js/dist/carousel.js
-// = ../../bower_components/bootstrap/js/dist/collapse.js
-// = ../../bower_components/bootstrap/js/dist/dropdown.js
-// = ../../bower_components/bootstrap/js/dist/modal.js
-// = ../../bower_components/bootstrap/js/dist/scrollspy.js
-// = ../../bower_components/bootstrap/js/dist/tab.js
-// = ../../bower_components/bootstrap/js/dist/tooltip.js
-// = ../../bower_components/bootstrap/js/dist/popover.js
+//= ../../bower_components/bootstrap/js/dist/util.js
+//= ../../bower_components/bootstrap/js/dist/alert.js
+//= ../../bower_components/bootstrap/js/dist/button.js
+//= ../../bower_components/bootstrap/js/dist/carousel.js
+//= ../../bower_components/bootstrap/js/dist/collapse.js
+//= ../../bower_components/bootstrap/js/dist/dropdown.js
+//= ../../bower_components/bootstrap/js/dist/modal.js
+//= ../../bower_components/bootstrap/js/dist/scrollspy.js
+//= ../../bower_components/bootstrap/js/dist/tab.js
+//= ../../bower_components/bootstrap/js/dist/tooltip.js
+//= ../../bower_components/bootstrap/js/dist/popover.js
 
