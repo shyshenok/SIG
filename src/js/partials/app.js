40e69d33be630500f1429eec3535758b22e13660
shyshenok <shyshenoka@gmail.com>
2017-12-04 10:24:44 +0200

    #5 Add scroll to bottom on arrow

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -49,6 +49,21 @@ $(document).ready(function() {
 
     });
 
+    $("[href='#products']").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 500, function(){
+
+                window.location.hash = hash;
+            });
+        }
+    });
+
 });
 
 function animateCounterHexagon() {
